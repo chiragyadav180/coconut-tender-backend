f5472d55a7244b2f37dd1d9fe32fa9efb70cd57e
BHAVISHEK1 <chiragy742@gmail.com>
2025-05-17 22:32:52 +0530

    new

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", 
+    origin: `${process.env.BASE_URL}`, 
     methods: ["GET", "POST", "PUT"]
   }
 });
